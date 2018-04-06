b36d41791e5dc341c4e96e4e71d250ccc88aa30c
Bart Donninger <bart.donninger@pggmenco.nl>
2018-04-06 14:29:52 +0200

    Add project files.

---
+++ Referer-Policy-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Referer_Policy_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
